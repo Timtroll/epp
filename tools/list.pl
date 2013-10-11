8e8d388f6b774336b6f3497b8a73fec1ea335b69
Timothy Zhouravlev <tim@data357.com>
2013-10-11 17:38:22 +0300

    New tool list.pl

--- tools/list.pl
+++ tools/list.pl
@@ -7,138 +7,83 @@ use MongoDB;
 use MongoDB::OID;
 use Net::EPP::Simple;
 
-use Tie::IxHash;
-use JSON::XS;
+# use Tie::IxHash;
+# use JSON::XS;
 
 # for developers
 use Data::Dumper;
 
+BEGIN {
+	IO::Socket::SSL::set_ctx_defaults(
+		'SSL_verify_mode' => 0 #'SSL_VERIFY_NONE'
+        );
+	$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = '0';
+};
+
+our (%conf, %collection, %months, %week, %in, %tmpl, %mesg, %domain_mail, %command_epp, %commands, %menu_line);
+
+require "../drs.pm";
+
 print "Content-type: text/html; charset = utf-8\nPragma: no-cache\n\n";
 
-my %conf = (
-	'give_mail'	=> 'auto-dbm@drs.net.ua',
-#	'give_mail'	=> 'troll@spam.net.ua',
-	'give_copy'	=> 'timtroll@yandex.ru',
-	'smtp'	=> '217.20.175.186',
-	'smtp_port'	=> '587',
-	'login_mail'	=> 'troll@spam.net.ua',
-	'pass_mail'	=> 'ghjuhfvvf',
-	'database'	=> 'mongodb://localhost,localhost;27017',
+# my %conf = (
+	# 'give_mail'	=> 'auto-dbm@drs.net.ua',
+# #	'give_mail'	=> 'troll@spam.net.ua',
+	# 'give_copy'	=> 'timtroll@yandex.ru',
+	# 'smtp'	=> '217.20.175.186',
+	# 'smtp_port'	=> '587',
+	# 'login_mail'	=> 'troll@spam.net.ua',
+	# 'pass_mail'	=> 'ghjuhfvvf',
+	# 'database'	=> 'mongodb://localhost,localhost;27017',
 
-	# preference URL`s
-	'public_url'	=> 'http://localhost/domain',
-	'public_css'	=> 'http://localhost/domain/css',
-	'public_cgi'	=> 'http://localhost/domain/drs.pl'
-);
+# # 	# preference URL`s
+	# 'public_url'	=> 'http://localhost/domain',
+	# 'public_css'	=> 'http://localhost/domain/css',
+	# 'public_cgi'	=> 'http://localhost/domain/drs.pl'
+# );
 
 my @list = ();
-open ('TMPL', "</var/www/domain/list.txt") ;
+open ('TMPL', "</var/www/epp/tools/list.txt") ;
 	while(<TMPL>){
 		chomp;
+		s/\,.*$//;
 		push @list, $_;
 	}
 close(TMPL);
 
-foreach my $list (@list) {
-	my $name = $list;
-	$list = `nslookup $list`;
-	$list =~ s/\n/ /go;
-	$list =~ s/.*answer//;
-	$list =~ /Address\:(.*)/;
-	$list = $1;
-	$list =~ s/ //go;
-	print "<li>$list = $name</li>";
-}
-exit;
 
-# my $json = JSON::XS->new();
 # Connect to Epp server
-my ($info, $resp);
-my $epp = Net::EPP::Simple->new(
-	host	=>'epp.uadns.com',
-	user	=>'trol-mnt-cunic',
-	pass	=>'hB2W2RzO86Fa'
-);
-
-#my $test = Net::EPP::Frame::Command::Poll::Req->new;
-#my $test = Net::EPP::Frame::Command::Poll::Ack->new;
-# $test->addChild($test);
-#my $t = $epp->request($test)->msg;
-#my $t = $epp->request($test)->code;
-#my $t = $epp->request($test)->resData;
-#my $t = $epp->request($test)->response;
-#my $t = $epp->request($test)->result;
-#my $t = $epp->request($test)->trID;
-#my $t = $epp->request($test)->clTRID;
-#my $t = $epp->request($test)->svTRID;
-#print Dumper($t);
-#exit;
+my $epp = &connect_epp();
 
 # Mongo
-my $connect = &connect('domains', 'domains_list');
-my %data = map { $_-> {'name'}, $_ } $connect -> find( {}, { 'name' => 1 } ) -> all;
+print "$conf{'database'}, $collection{'domains'}\n\n";
+my $connect = &connect($conf{'database'}, $collection{'domains'});
 
-foreach (keys %data) {
-	print "$_ = ";
-	print $data{$_}->{'exDate'};
-	print " = ";
-$data{$_}->{'date'} = &sec2date(&date2sec($data{$_}->{'exDate'}), 'md');
-$data{$_}->{'expires'} = &date2sec($data{$_}->{'exDate'});
-	print " = ";
-	print $data{$_}->{'date'};
-	print "<br>";
-	$connect->update( { '_id' => $data{$_}->{'_id'} }, { '$set' => { 'date' =>$data{$_}->{'date'}, 'expires' =>$data{$_}->{'expires'} } } );
+
+foreach my $key (@list) {
+	my $count = $connect -> find( {'name' => $key}, { 'name' => 1 } ) -> count;
+	unless ($count) {
+		print "$key = $count\n";
+		my $info = $epp -> domain_info($key);
+		print Dumper($info);
+		if ($key =~ /wushu.sebastopol.ua/) {
+		last;
+		}
+		$info->{'date'} = &sec2date(&date2sec($info->{'crDate'}), 'md');
+		$info->{'expires'} = &date2sec($info->{'exDate'});
+		$info->{'type'} = 'use';
+		print "\n\n\n";
+	}
 }
 exit;
-foreach my $list (@list) {
-	if (exists $data{$list}) {
-		next;
-	}
-	# check domain
-	$resp = $epp->check_domain($list);
-print "<li>$resp = $list</li>";
-	my $cnt = 0;
-	if ($resp) {
-		print "Домен свободен";
-	}
-	else {
-		$info = undef;
-		my $t = undef;
-LOOP:	$info = $epp->domain_info($list);
-		unless ($info) {
-			if ($cnt <50) {
-				$cnt++;
-				sleep 1;
-				goto LOOP;
-			}
-			else {
-				print "<hr><b>Свалился на домене $list<b>";
-			}
-		}
-print Dumper($info);
-print $info->{'exDate'}, "<br>";
-$info->{'date'} = &sec2date(&date2sec($info->{'exDate'}), 'md');
-$info->{'expires'} = &date2sec($info->{'exDate'});
-#print Dumper($info);
-		$connect->insert($info);
-	}
-#	print "$list\n<br>";
-#print $resp;
-#	last;
-	sleep 1;
-}
 
 sub date2sec {
 	my ($date, $sec);
 	$date = shift;
 
 	$date =~ /(\d{4}?)\-(\d{2}?)\-(\d{2}?).(\d{2}?)\:(\d{2}?)\:(\d{2}?)/;
-#	print "$3 $2 $1 $4 $5 $6<br>";
-#	$sec = timelocal($6, $5, $4, $3, $2, $1);
 	$sec = timelocal($6, $5, $4, $3, ($2-1), $1);
-#print "$sec<br>";
-#	print &sec2date($sec);
-#print "<br>$sec<br>";
+
 	return $sec;
 }
 
@@ -156,9 +101,20 @@ sub sec2date {
 	$tmp[4] = ($tmp[4]+1);
 	if ($tmp[4] < 10) { $tmp[4] ='0'.$tmp[4]; }
 
+	# 1101 (month+day)
 	if ($sep eq 'md') {
 		$date = $tmp[4].$tmp[3];
 	}
+	# 2011-12-06T08:53:24.0948Z
+	elsif ($sep eq 'iso') {
+		$date = ($tmp[5]+1900)."-$tmp[4]-$tmp[3]T$tmp[2]:$tmp[1]:$tmp[0].1111Z";
+	}
+	# 2001-12-01 (yy-mm-dd where '-' is separeator)
+	elsif ($sep eq 'date') {
+		$date = ($tmp[5]+1900)."-".$tmp[4]."-".$tmp[3];
+#		$date = ($tmp[5]+1900)."-".$tmp[3]."-".$tmp[4];
+	}
+	# 01-02-2001 (dd-mm-yy where '-' is separeator)
 	else {
 		$date = $tmp[3].$sep.$tmp[4].$sep.($tmp[5]+1900);
 	}
@@ -173,13 +129,35 @@ sub connect {
 	$col = shift;
 
 	# Set collection name if not exists
-	unless ($base) { $base = 'domains'; }
-	unless ($col) { $col = 'domains'; }
+	unless ($base) { $base = $conf{'database'}; }
+	unless ($col) { $col = $collection{'domains'}; }
 
 	# Read list of domains
-	$client = MongoDB::MongoClient -> new(host => $conf{'database'});
-	$db = $client -> get_database( $base );
+#	$client = MongoDB::MongoClient->new(host => $conf{'db_link'});
+	$client = MongoDB::Connection->new(host => $conf{'db_link'});
+	$db = $client->get_database( $base );
 	$collections = $db->get_collection( $col);
 
 	return $collections;
 }
+
+sub connect_epp {
+	my ($epp);
+
+	# Connect to Epp server
+	$epp = Net::EPP::Simple->new(
+		host		=> $conf{'epp_host'},
+		user		=> $conf{'epp_user'},
+		timeout	=> $conf{'epp_timeout'},
+		pass		=> $conf{'epp_pass'},
+		debug	=> $conf{'debug_epp'}
+	);
+
+print $Net::EPP::Simple::Error;
+	# if (($Net::EPP::Simple::Code == 2500)||($Net::EPP::Simple::Code == 2501)||($Net::EPP::Simple::Code == 2502)) {
+		# $in{'messages'} = $Net::EPP::Simple::Message."<br>".$Net::EPP::Simple::Error;
+	# }
+
+	return $epp;
+}
+
